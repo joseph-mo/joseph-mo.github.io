ac2c028aadf6f315c15e5ef7f9f76ad82bf4b5fd
Joseph <josephmo@josephs-air.myfiosgateway.com>
2022-04-15 13:00:49 -0400

    Cleaning up routing and adding 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import Work from './components/main/Work';
 import About from './components/main/About';
 import Navigation from './components/main/Navigation';
 import ProjectDetail from './components/projects/ProjectDetail';
+import NotFoundPage from './components/main/NotFoundPage';
 
 const App = () => {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="" element={<Work />} />
-        <Route path="/projects" element={<Work />} />
-        <Route path="/work" element={<Work />} />
+        <Route path="/" element={<Work />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects/:projectName" element={<ProjectDetail />} />
-        {/* Determines routing if path not found         
-        <Route path="*" /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
